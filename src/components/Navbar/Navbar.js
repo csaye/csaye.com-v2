cf15f69e5a0a15f9a03a3c26d9f31f3b9d20512b
Cooper Saye <coopersaye@gmail.com>
2021-05-24 14:55:03 -0700

    Create Navbar component.

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+      <a className="nav-link" href="/">HOME</a>
+      <a className="nav-link" href="projects">PROJECTS</a>
+      <a className="nav-link" href="music">MUSIC</a>
+    </div>
+  );
+}
+
+export default Navbar;
