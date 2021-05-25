5b6850743f75ad93e77a1f5670a7e756f1852598
Cooper Saye <coopersaye@gmail.com>
2021-05-24 23:08:36 -0700

    Import project data.

---
+++ src/data/projects.js
@@ -0,0 +1,540 @@
+export const projects = [
+  {
+    name: "DevComms",
+    description: "Team communication and coordination all in one place.",
+    link: "https://csaye.com/devcomms",
+    image: "devcomms.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.com/devcomms"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/devcomms"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Firebase",
+      "CSS",
+      "Atom"
+    ]
+  },
+  {
+    name: "FlexScript",
+    description: "A programming language that compiles into multiple languages.",
+    link: "https://github.com/csaye/flexscript",
+    image: "flexscript.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://github.com/csaye/flexscript#installationcompilation"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/flexscript"
+      }
+    ],
+    icons: [
+      "Python",
+      "Shell",
+      "Atom",
+      "C#",
+      "Java",
+      "C++",
+      "JavaScript"
+    ]
+  },
+  {
+    name: "ImgScript",
+    description: "An image-based programming language.",
+    link: "https://github.com/csaye/imgscript",
+    image: "imgscript.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://github.com/csaye/imgscript#installationcompilation"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/imgscript"
+      }
+    ],
+    icons: [
+      "Python",
+      "Atom"
+    ]
+  },
+  {
+    name: "Calendar",
+    description: "Event scheduling in a simple interface.",
+    link: "https://csaye.com/calendar",
+    image: "calendar.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.com/calendar"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/calendar"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Firebase",
+      "CSS",
+      "Atom"
+    ]
+  },
+  {
+    name: "SimpleStore",
+    description: "File storage simplified.",
+    link: "https://csaye.com/simple-store",
+    image: "storage.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.com/simple-store"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/simple-store"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Firebase",
+      "CSS",
+      "Atom"
+    ]
+  },
+  {
+    name: "Mono3D",
+    description: "A 3D raycasting engine built in MonoGame.",
+    link: "https://github.com/csaye/mono3d",
+    image: "grid.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://github.com/csaye/mono3d#installationrunning"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/mono3d"
+      }
+    ],
+    icons: [
+      "MonoGame",
+      "C#",
+      "Visual Studio"
+    ]
+  },
+  {
+    name: "Mono2D",
+    description: "A 2D raycasting engine built in MonoGame.",
+    link: "https://github.com/csaye/mono2d",
+    image: "pillars.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://github.com/csaye/mono2d#installationrunning"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/mono2d"
+      }
+    ],
+    icons: [
+      "MonoGame",
+      "C#",
+      "Visual Studio"
+    ]
+  },
+  {
+    name: "Meta Python",
+    description: "Python written in Python.",
+    link: "https://github.com/csaye/meta-python",
+    image: "meta.png",
+    buttons: [
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/meta-python"
+      }
+    ],
+    icons: [
+      "Python",
+      "IDLE"
+    ]
+  },
+  {
+    name: "Widget Wall",
+    description: "A webpage with a collection of useful widgets.",
+    link: "https://csaye.com/widget-wall",
+    image: "widgets.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.com/widget-wall"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/widget-wall"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Firebase",
+      "CSS",
+      "Datamuse API",
+      "Bit.ly API",
+      "OpenWeather API",
+      "Random Word API",
+      "Atom"
+    ]
+  },
+  {
+    name: "Python Highlights",
+    description: "Highlights from my personal Python projects.",
+    link: "https://github.com/csaye/python-highlights",
+    image: "highlights.png",
+    buttons: [
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/python-highlights"
+      }
+    ],
+    icons: [
+      "Python",
+      "Pygame",
+      "IDLE"
+    ]
+  },
+  {
+    name: "MinScript",
+    description: "A minimalist programming language written in Python.",
+    link: "https://github.com/csaye/minscript",
+    image: "minscript.png",
+    buttons: [
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/minscript"
+      }
+    ],
+    icons: [
+      "Python",
+      "IDLE"
+    ]
+  },
+  {
+    name: "Countdown Tracker",
+    description: "A countdown tracker tool.",
+    link: "https://csaye.com/countdown-tracker",
+    image: "countdown2.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.com/countdown-tracker"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/countdown-tracker"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Firebase",
+      "CSS",
+      "Atom"
+    ]
+  },
+  {
+    name: "Bicycle",
+    description: "A minimalist social media placing its users first.",
+    link: "https://bicycle.social",
+    image: "bicycle.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://bicycle.social"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/bicycle"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Firebase",
+      "CSS",
+      "Atom"
+    ]
+  },
+  {
+    name: "Playlist Bot",
+    description: "Randomly generate Spotify playlists.",
+    link: "https://csaye.com/playlist-bot",
+    image: "playlist.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.com/playlist-bot"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/playlist-bot"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Spotify API",
+      "CSS",
+      "Atom"
+    ]
+  },
+  {
+    name: "Deserted",
+    description: "Forage, build, and innovate on your new island. A creative, ambient survival game.",
+    link: "none",
+    image: "astar.png",
+    buttons: [
+      {
+        label: "Available Now",
+        link: "https://csaye.itch.io/deserted"
+      },
+      {
+        label: "Soundtrack",
+        link: "https://soundcloud.com/csaye/sets/deserted"
+      }
+    ],
+    icons: [
+      "Unity",
+      "C#",
+      "VSCode",
+      "FL Studio",
+      "Audacity",
+      "Aseprite",
+      "GIMP"
+    ]
+  },
+  {
+    name: "Ticket Tracker",
+    description: "A ticket tracker tool.",
+    link: "https://csaye.com/ticket-tracker",
+    image: "tickets.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.com/ticket-tracker"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/ticket-tracker"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Firebase",
+      "CSS",
+      "Atom"
+    ]
+  },
+  {
+    name: "Coding Challenges",
+    description: "A collection of common coding challenges in various languages.",
+    link: "https://github.com/csaye/coding-challenges",
+    image: "challenges.png",
+    buttons: [
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/coding-challenges"
+      }
+    ],
+    icons: [
+      "C#",
+      "Python",
+      "Java",
+      "C++",
+      "JavaScript",
+      "C",
+      "Haskell"
+    ]
+  },
+  {
+    name: "Jamming",
+    description: "A spotify playlist builder.",
+    link: "https://csaye.com/jamming",
+    image: "jamming.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.com/jamming"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/jamming"
+      }
+    ],
+    icons: [
+      "React.js",
+      "Spotify API",
+      "CSS",
+      "Atom"
+    ]
+  },
+  {
+    name: "Countdown",
+    description: "A lightweight, downloadable countdown tracker.",
+    link: "https://csaye.itch.io/countdown",
+    image: "countdown.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.itch.io/countdown"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/countdown"
+      }
+    ],
+    icons: [
+      "Unity",
+      "C#",
+      "VSCode"
+    ]
+  },
+  {
+    name: "2048 Live",
+    description: "\"2048\" controlled through real-time object detection.",
+    link: "none",
+    image: "astar.png",
+    buttons: [
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/2048-live"
+      }
+    ],
+    icons: [
+      "C++",
+      "SDL2",
+      "OpenCV",
+      "VSCode"
+    ]
+  },
+  {
+    name: "Sorting Visualization",
+    description: "An interactive visualization tool for eight common sorting algorithms.",
+    link: "https://csaye.itch.io/sorting-visualization",
+    image: "sorting.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.itch.io/sorting-visualization"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/sorting-visualization"
+      }
+    ],
+    icons: [
+      "Unity",
+      "C#",
+      "VSCode"
+    ]
+  },
+  {
+    name: "Chess",
+    description: "Play chess against a basic AI.",
+    link: "https://csaye.itch.io/chess",
+    image: "chess.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.itch.io/chess"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/chess"
+      }
+    ],
+    icons: [
+      "Unity",
+      "C#",
+      "VSCode",
+      "GIMP"
+    ]
+  },
+  {
+    name: "A* Visualization",
+    description: "An interactive step-by-step visualization of the A* pathfinding algorithm.",
+    link: "https://csaye.itch.io/a-star-visualization",
+    image: "astar.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.itch.io/a-star-visualization"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/a-star-visualization"
+      }
+    ],
+    icons: [
+      "Unity",
+      "C#",
+      "VSCode",
+      "GIMP"
+    ]
+  },
+  {
+    name: "Diapixel",
+    description: "A minimal 2D and 3D diagramming tool built in a day.",
+    link: "https://csaye.itch.io/diapixel",
+    image: "diapixel.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.itch.io/diapixel"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/diapixel"
+      }
+    ],
+    icons: [
+      "Unity",
+      "C#",
+      "VSCode",
+      "GIMP"
+    ]
+  },
+  {
+    name: "Computeroid",
+    description: "Crash the computer — a top-down puzzle game with a retro computer theme.",
+    link: "https://csaye.itch.io/computeroid",
+    image: "computeroid.png",
+    buttons: [
+      {
+        label: "Try it out",
+        link: "https://csaye.itch.io/computeroid"
+      },
+      {
+        label: "GitHub",
+        link: "https://github.com/csaye/computeroid"
+      },
+      {
+        label: "Soundtrack",
+        link: "https://soundcloud.com/csaye/sets/computeroid"
+      }
+    ],
+    icons: [
+      "Unity",
+      "C#",
+      "VSCode",
+      "FL Studio",
+      "BFXR",
+      "GIMP"
+    ]
+  }
+];
