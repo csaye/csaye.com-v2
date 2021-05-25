190c45f89876c825f917f07d2affeb89a5066fce
Cooper Saye <coopersaye@gmail.com>
2021-05-25 15:03:48 -0700

    Implement project filtering.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import './Projects.css';
 
 import { projects } from '../../data/projects.js';
@@ -6,13 +8,60 @@ import Title from '../Title/Title.js';
 import Project from './Project/Project.js';
 
 function Projects() {
+  const [webActive, setWebActive] = useState(true);
+  const [gameActive, setGameActive] = useState(true);
+  const [otherActive, setOtherActive] = useState(true);
+
+  // returns whether given project type is currently active
+  function isActive(type) {
+    return (type === 'web' && webActive) ||
+      (type === 'game' && gameActive) ||
+      (type === 'other' && otherActive);
+  }
+
   return (
     <div className="Projects">
+      <span id="projects" className="scrollto" />
       <Title title="Projects" />
+      <div className="options">
+        <label htmlFor="checkbox-webactive">
+          <i className="fas fa-laptop-code icon-blue" />web
+        </label>
+        <input
+          id="checkbox-webactive"
+          type="checkbox"
+          checked={webActive}
+          onChange={e => setWebActive(e.target.checked)}
+        />
+        |
+        <label htmlFor="checkbox-gameactive">
+          <i className="fas fa-gamepad icon-green" />game
+        </label>
+        <input
+          id="checkbox-gameactive"
+          type="checkbox"
+          checked={gameActive}
+          onChange={e => setGameActive(e.target.checked)}
+        />
+        |
+        <label htmlFor="checkbox-otheractive">
+          <i className="fas fa-plus icon-red" />other
+        </label>
+        <input
+          id="checkbox-otheractive"
+          type="checkbox"
+          checked={otherActive}
+          onChange={e => setOtherActive(e.target.checked)}
+        />
+      </div>
       <div className="projects-list">
         {
-          projects.map((project, i) =>
-            <Project key={`project-${i}`} data={project} id={i} />
+          projects.filter(p => isActive(p.type)).map((p, i) =>
+            <Project
+              key={`project-${i}`}
+              data={p}
+              id={i}
+            />
           )
         }
       </div>
