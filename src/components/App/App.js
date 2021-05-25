e96d7e44a129dd3325921b6ea8c27f8f36f84f0d
Cooper Saye <coopersaye@gmail.com>
2021-05-25 11:39:51 -0700

    Create Footer component.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Navbar from '../Navbar/Navbar.js';
 import Home from '../Home/Home.js';
 import Projects from '../Projects/Projects.js';
 import Music from '../Music/Music.js';
+import Footer from '../Footer/Footer.js';
 
 import './App.css';
 
@@ -12,11 +13,14 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Switch>
-          <Route path="/projects"><Projects /></Route>
-          <Route path="/music"><Music /></Route>
-          <Route path="/"><Home /></Route>
-        </Switch>
+        <div className="content">
+          <Switch>
+            <Route path="/projects"><Projects /></Route>
+            <Route path="/music"><Music /></Route>
+            <Route path="/"><Home /></Route>
+          </Switch>
+        </div>
+        <Footer />
       </Router>
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="Footer">
+      © Cooper Saye {new Date().getFullYear()}
+    </div>
+  );
+}
+
+export default Footer;
