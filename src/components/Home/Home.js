1f01f31467b6aae4af8da080fc1287adc502810a
Cooper Saye <coopersaye@gmail.com>
2021-05-25 13:43:10 -0700

    Restructure Home component.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,19 @@
 import Title from '../Title/Title.js';
+import Contact from '../Contact/Contact.js';
+import Skillset from '../Skillset/Skillset.js';
+import Projects from '../Projects/Projects.js';
+
+import { projects } from '../../data/projects.js';
 
 import './Home.css';
 
 function Home() {
   return (
     <div className="Home">
-      <Title title="Cooper Saye" subtitle="web and game developer" />
+      <Title main={true} title="Cooper Saye" subtitle="web and game developer" />
+      <Contact />
+      <Skillset />
+      <Projects />
     </div>
   );
 }
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,12 +1,13 @@
 import './Title.css';
 
 function Title(props) {
+  const main = props.main;
   const title = props.title;
   const subtitle = props.subtitle;
 
   return (
     <div className="Title">
-      <h1>{title}</h1>
+      {main ? <h1 className="main">{title}</h1> : <h1>{title}</h1>}
       {subtitle && <h2>{subtitle}</h2>}
       <hr />
     </div>
