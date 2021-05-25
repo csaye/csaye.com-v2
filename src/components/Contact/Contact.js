5c86c5ca8e5d25a1a4e5a7a2a40afa63630df8d6
Cooper Saye <coopersaye@gmail.com>
2021-05-25 15:04:34 -0700

    Create Contact and Skillset components.

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,14 @@
+import Title from '../Title/Title.js';
+
+import './Contact.css';
+
+function Contact() {
+  return (
+    <div className="Contact">
+      <span id="contact" className="scrollto" />
+      <Title title="Contact" />
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/Skillset/Skillset.js
@@ -0,0 +1,16 @@
+import Title from '../Title/Title.js';
+
+import { skillset } from '../../data/skillset.js';
+
+import './Skillset.css';
+
+function Skillset() {
+  return (
+    <div className="Skillset">
+      <span id="skillset" className="scrollto" />
+      <Title title="Skillset" />
+    </div>
+  );
+}
+
+export default Skillset;
