2fe31b65bf4fad9491228827f731428c1f51c758
Cooper Saye <coopersaye@gmail.com>
2021-05-24 17:11:01 -0700

    Create Project component.

---
+++ src/components/Projects/Project/Project.js
@@ -0,0 +1,49 @@
+import Icon from '../../Icon/Icon.js';
+
+import './Project.css';
+
+function Project(props) {
+  const id = props.id;
+  const { name, description, link, image, buttons, icons } = props.data;
+
+  return (
+    <div className="Project">
+      <h2>{name}</h2>
+      <p>{description}</p>
+      <a
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          src={require(`../../../images/projects/${image}`).default}
+          onClick={() => window.open(link)}
+          alt={image}
+        />
+      </a>
+      <div>
+        {
+          buttons.map((button, i) =>
+            <a
+              key={`project-${id}-button-${i}`}
+              href={button.link}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {button.label}
+            </a>
+          )
+        }
+      </div>
+      <div className="icon-list">
+        {
+          icons.map((icon, i) =>
+            <Icon key={`project-${id}-icon-${i}`} icon={icon} />
+          )
+        }
+      </div>
+    </div>
+  );
+}
+
+export default Project;
