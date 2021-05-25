f9cefa4472fe653b25d36ecac58669d0068fe980
Cooper Saye <coopersaye@gmail.com>
2021-05-25 13:31:54 -0700

    Remove react router.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,5 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
-import Navbar from '../Navbar/Navbar.js';
+import Header from '../Header/Header.js';
 import Home from '../Home/Home.js';
-import Projects from '../Projects/Projects.js';
-import Music from '../Music/Music.js';
 import Footer from '../Footer/Footer.js';
 
 import './App.css';
@@ -11,17 +7,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <div className="content">
-          <Switch>
-            <Route path="/projects"><Projects /></Route>
-            <Route path="/music"><Music /></Route>
-            <Route path="/"><Home /></Route>
-          </Switch>
-        </div>
-        <Footer />
-      </Router>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
