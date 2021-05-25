816c4c2d7c46e096610cd86c54239c66200c901e
Cooper Saye <coopersaye@gmail.com>
2021-05-24 22:03:27 -0700

    Improve projects page styling.

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,17 +2,20 @@ import './Projects.css';
 
 import { projects } from '../../data/projects.js';
 
+import Title from '../Title/Title.js';
 import Project from './Project/Project.js';
 
 function Projects() {
   return (
     <div className="Projects">
-      <h1>Projects</h1>
-      {
-        projects.map((project, i) =>
-          <Project key={`project-${i}`} data={project} id={i} />
-        )
-      }
+      <Title title="Projects" />
+      <div className="projects-list">
+        {
+          projects.map((project, i) =>
+            <Project key={`project-${i}`} data={project} id={i} />
+          )
+        }
+      </div>
     </div>
   );
 }
