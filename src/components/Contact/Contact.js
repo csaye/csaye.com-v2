e3c03e4580959cbdddab732d6695583cced65234
Cooper Saye <coopersaye@gmail.com>
2021-05-25 23:00:08 -0700

    Implement contacts display.

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,32 @@
-import Title from '../Title/Title.js';
+import { contacts } from '../../data/contacts.js';
 
 import './Contact.css';
 
 function Contact() {
   return (
     <div className="Contact">
-      <span id="contact" className="scrollto" />
-      <Title title="Contact" />
+      <div className="contacts">
+        {
+          contacts.map((c, i) =>
+            <div key={`contact-${i}`} className="contact-div">
+              {
+                c.email &&
+                <a href={`mailto:${c.email}`}>
+                  {c.icon}
+                  <p>{c.text}</p>
+                </a>
+              }
+              {
+                c.link &&
+                <a href={c.link} target="_blank" rel="noreferrer">
+                  {c.icon}
+                  <p>{c.text}</p>
+                </a>
+              }
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
