88e5bfea294b3a13621550cc1436c6f8c74fe1e4
Cooper Saye <coopersaye@gmail.com>
2021-05-27 13:24:06 -0700

    Implement darkmode toggle.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,25 @@ import './Header.css';
 
 import logo from '../../images/logo.png';
 
+// get darkmode stylesheet
+const stylesheet = document.getElementById("stylesheet-darkmode");
+stylesheet.disabled = true;
+
 function Header() {
+  // toggles darkmode stylesheet
+  function setDarkmode(isDarkmode) {
+    stylesheet.disabled = !isDarkmode;
+  }
+
   return (
     <div className="Header">
       <a href="#home">
         <img src={logo} alt="logo" />
       </a>
+      <label className="switch">
+        <input type="checkbox" onChange={e => setDarkmode(e.target.checked)} />
+        <span className="slider" />
+      </label>
       <span className="flex-fill" />
       <a className="link" href="#home">HOME</a>
       <a className="link" href="#skillset">SKILLSET</a>
